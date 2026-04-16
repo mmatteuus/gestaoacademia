6c84df24169d56e75b7c7bff3cf10a44b83cc874
MATEUS <138159754+mmatteuus@users.noreply.github.com>
2026-04-15 21:51:48 -0300

    feat: conectar provider operacional ao app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AdminLayout } from "@/components/layout/AdminLayout";
 import { AcademiaDataProvider } from "@/features/academia/AcademiaDataProvider";
+import { OperacionalDataProvider } from "@/features/operacional/OperacionalDataProvider";
 import DashboardPage from "./pages/DashboardPage";
 import AlunosPage from "./pages/AlunosPage";
 import TurmasPage from "./pages/TurmasPage";
@@ -25,25 +26,27 @@ const App = () => (
     <TooltipProvider>
       <Sonner />
       <AcademiaDataProvider>
-        <BrowserRouter>
-          <AdminLayout>
-            <Routes>
-              <Route path="/" element={<DashboardPage />} />
-              <Route path="/alunos" element={<AlunosPage />} />
-              <Route path="/turmas" element={<TurmasPage />} />
-              <Route path="/frequencia" element={<FrequenciaPage />} />
-              <Route path="/graduacao" element={<GraduacaoPage />} />
-              <Route path="/ranking" element={<RankingPage />} />
-              <Route path="/campeonatos" element={<CampeonatosPage />} />
-              <Route path="/financeiro" element={<FinanceiroPage />} />
-              <Route path="/financeiro-gerencial" element={<FinanceiroGerencialPage />} />
-              <Route path="/produtos" element={<ProdutosPage />} />
-              <Route path="/aluguel" element={<AluguelPage />} />
-              <Route path="/relatorios" element={<RelatoriosPage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </AdminLayout>
-        </BrowserRouter>
+        <OperacionalDataProvider>
+          <BrowserRouter>
+            <AdminLayout>
+              <Routes>
+                <Route path="/" element={<DashboardPage />} />
+                <Route path="/alunos" element={<AlunosPage />} />
+                <Route path="/turmas" element={<TurmasPage />} />
+                <Route path="/frequencia" element={<FrequenciaPage />} />
+                <Route path="/graduacao" element={<GraduacaoPage />} />
+                <Route path="/ranking" element={<RankingPage />} />
+                <Route path="/campeonatos" element={<CampeonatosPage />} />
+                <Route path="/financeiro" element={<FinanceiroPage />} />
+                <Route path="/financeiro-gerencial" element={<FinanceiroGerencialPage />} />
+                <Route path="/produtos" element={<ProdutosPage />} />
+                <Route path="/aluguel" element={<AluguelPage />} />
+                <Route path="/relatorios" element={<RelatoriosPage />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </AdminLayout>
+          </BrowserRouter>
+        </OperacionalDataProvider>
       </AcademiaDataProvider>
     </TooltipProvider>
   </QueryClientProvider>
